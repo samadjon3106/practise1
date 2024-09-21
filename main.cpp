efae42580f2a81be62517f0af20a455f38822871
samadjon3106 <s.sayfullayev@newuu.uz>
2024-09-21 16:35:10 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    //double x = 3, y=4 , z;
+    //z= min(x,y);
+    //z=max(x,y);
+    //z=pow(2, 4);
+    //z= sqrt(16);
+    //z= abs(-10);
+    //z=round(4);
+    //z=ceil(2.56);
+    //z=floor(2.74);
+    /*double a,b,c;
+       cout<<"Enter side A:";
+       cin>>a;
+
+       cout<<"Enter side B:";
+       cin>>b;
+
+       a=pow(a,2);
+       b=pow(b,2);
+       c=sqrt(pow(a,2)+pow(b, 2));
+       cout<<"side C: "<<c;;
+       return 0;*/
+
+    //if statement
+    /*int age;
+    cin>>age;
+
+    if ( age>= 18) {
+        cout<< " you can participate in voting";
+
+    }else {
+        cout << " You are not eligible for voting"<<endl;
+    }
+
+        return 0;*/
+
+    // switch
+
+/*int students_name;
+    cout<< " Enter the student's name: ";
+    cin>> students_name;
+
+    switch (students_name) {
+        case 'Samadjon':
+            cout<< " he is a leader";
+        break;
+        case 'Abdurahmon':
+            cout<< " he is good boy";
+        break;
+        case ' Asror':
+            cout<< " he is strong";
+        break;
+        case ' Farangiz':
+            cout<<" She is beautiful";
+        break;
+        default:
+            cout<< "please enter written name";
+
+        return 0;
+
+    }*/\
+
+  // calculator
+    /*char op;
+    double num1;
+    double num2;
+    double result;
+    cout<< " Calculator";
+    cout<<" enter ( + - * /):";
+    cin>>op;
+
+    cout<<"Enter num1: ";
+    cin>> num1;
+
+    cout<<" Enter num2: ";
+    cin>>num2;
+
+    switch (op) {
+        case '+':
+        result=num1+num2;
+        cout<< "result: " << result;
+        break;
+        case '-':
+            result=num1-num2;
+        cout<< "result: " << result;
+        break;case '*':
+            result=num1*num2;
+        cout<< "result: " << result;
+        break;case '/':
+            result=num1/num2;
+        cout<< "result: " << result;
+        break;
+        default:
+            cout<< "please enter the true thing";
+        return 0;
+
+
+    }*/
+}
+
+
+
+
+
+
